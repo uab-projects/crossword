b448d6bdcb95a46fd1c38ecd27a7dd3774b1bcd3
David LJ <davidlj@smartlysoft.com>
2016-09-22 09:49:52 +0200

    separed loadEnvironment and added comments

--- src/main.py
+++ src/main.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Loads the crossword into a list of lists with each item being a line of the
+crossword
 
-def loadEnvironment():
-    #TODO: to read crossword table
-
-    #TODO: to read dictionary
-    wordlist = [line.rstrip('\n') for line in open('Material/diccionari_CB.txt', 'r')]
-    crossword = [line.rstrip('\n') for line in open('Material/crossword_CB.txt', 'r')]
+@param  filename    file to load
+@return crossword loaded
+"""
+def loadCrossword(filename):
+    crossword = [line.rstrip('\n') for line in open(filename, 'r')]
     crossword = list(map(lambda x: x.split("\t"),crossword))
+    return crossword
+
+"""
+Loads the wordlist into a list given the name of the file where is stored the
+dictionary
+
+The dictionary must be formated with one word per line. Thanks :)
+
+@param  filename    file to load
+@return wordlist as a list of strings (words)
+"""
+def loadWordlist(filename):
+    return [line.rstrip('\n') for line in open(filename, 'r')]
 
 """
 Returns the domain for the given variable
